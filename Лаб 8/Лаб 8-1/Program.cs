e961db2ebc8513c60aa910a10c4010f2c11722e3
KarakulGek <Lebed.Daniil2005@gmail.com>
2023-10-14 13:40:08 +0300

    изменение 2

--- Лаб 8/Лаб 8-1/Program.cs
+++ Лаб 8/Лаб 8-1/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //Изменение №1
             Console.Write("Введите количество команд: ");
             int n = int.Parse(Console.ReadLine());
             Team[] teams = new Team[n];
